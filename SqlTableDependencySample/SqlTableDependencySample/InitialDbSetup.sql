d2be747f4e04fc3f87ee4abab2a21b334c2e1fc3
VIGNESH SUBRAMANI <subramani.vignesh@jgc.com>
2019-07-17 16:27:10 +0900

    SqlTableDependency

---
+++ SqlTableDependencySample/SqlTableDependencySample/InitialDbSetup.sql
@@ -0,0 +1,29 @@
+﻿USE [Customer]
+
+CREATE TABLE [dbo].[Customers](
+	[Id] [bigint] NOT NULL,
+	[First Name] [varchar](50) NULL,
+	[Code] [int] NULL,
+	[City] [varchar](50) NULL,
+	[Last Name] [varchar](50) NULL,
+	[Country] [varchar](50) NULL
+) ON [PRIMARY]
+
+Insert into Customers Values (1,'Peter',123,'Los Angeles','Hein','America')
+Insert into Customers Values (2,'Francois',456,'Paris','Gilles','France')
+Insert into Customers Values (3,'Kevin',789,'London','Peterson','United Kingdom')
+Insert into Customers Values (4,'Mark',134,'Toronto','Henry','Canada')
+Insert into Customers Values (5,'Sakurai',341,'Tokyo','Shoko','Japan')
+Insert into Customers Values (6,'Jim',734,'Shanghai','Lee','China')
+Insert into Customers Values (7,'Karl',946,'Queenstown','Max','New Zealand')
+Insert into Customers Values (8,'James',627,'Manila','Max','Philippines')
+Insert into Customers Values (9,'Mohammad',639,'Karachi','Khan','Pakistan')
+
+
+ALTER DATABASE Customer SET ENABLE_BROKER WITH ROLLBACK IMMEDIATE;
+
+
+--Testing purpose
+Insert into Customers Values (10,'Rashid',295,'Barisal','Akmal','Bangladesh')
+DELETE FROM Customers where Id=10
+Update Customers set [Last Name]='Wood' where Id=7
---
+++ SqlTableDependencySample/SqlTableDependencySample/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace SqlTableDependencySample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string _con = @"data source=(LocalDb)\MSSQLLocalDB; initial catalog=Customer; integrated security=True";
+        private SqlTableDependency<Customer>  dep;
+        public ObservableCollection<Customer> GridSource { get; set; } = new ObservableCollection<Customer>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            var mapper = new ModelToTableMapper<Customer>();
+            mapper.AddMapping(c => c.Surname, "Last Name");
+            mapper.AddMapping(c => c.Name, "First Name");
+            
+                dep = new SqlTableDependency<Customer>(_con, "Customers", mapper: mapper);
+
+                dep.OnChanged += Changed;
+                dep.Start();
+
+            using (SqlConnection connection = new SqlConnection(_con))
+            using (SqlCommand command = new SqlCommand("Select * from Customers", connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var cust = new Customer();
+                        cust.Id = Convert.ToInt32(reader["Id"]);
+                        cust.Name = reader["First Name"].ToString();
+                        cust.Surname = reader["Last Name"].ToString();
+                        GridSource.Add(cust);
+                    }
+                }
+            }
+
+            this.Closed += MainWindow_Closed;
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            dep.Stop();
+        }
+
+        public void Changed(object sender, RecordChangedEventArgs<Customer> e)
+        {
+            var changedEntity = e.Entity;
+
+            if(e.ChangeType == TableDependency.SqlClient.Base.Enums.ChangeType.Insert)
+                Application.Current.Dispatcher.Invoke(() => { GridSource.Add(changedEntity); });
+            else if(e.ChangeType == TableDependency.SqlClient.Base.Enums.ChangeType.Delete)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    var entity = GridSource.FirstOrDefault(x => x.Id == changedEntity.Id);
+                    GridSource.Remove(entity);
+                }
+                );
+            }
+            else
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    var entity = GridSource.FirstOrDefault(x => x.Id == changedEntity.Id);
+                    GridSource.Remove(entity);
+                    GridSource.Add(changedEntity);
+                });
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
