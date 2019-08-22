843ca9e1d22aed3639d6e30bc9959e2805b36493
VIGNESH SUBRAMANI <subramani.vignesh@jgc.com>
2019-08-22 19:23:29 +0900

    SQL Assembly

---
+++ SqlAssembly/SqlAssembly/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+
+namespace SqlAssembly
+{
+    public class Customer
+    {
+        [Microsoft.SqlServer.Server.SqlFunction(FillRowMethodName = "FillNameRow", TableDefinition = "Name nvarchar(max)")]
+        public static IEnumerable GetCustomerNames()
+        {
+            return new List<string> { "Mark", "Jack", "Peter", "Henry", "Wood", "Mark"};
+        }
+
+        public static void FillNameRow(object name, out SqlString Name)
+        {
+            Name = (string)name;
+        }
+    }
+}
